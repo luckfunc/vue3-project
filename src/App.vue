98ea7aa184b49946a8d4ecf1e350f5e8a9b596d2
顶级压力小子 <45672942@qq.com>
2023-10-19 00:21:29 +0800

    feat: setup

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
 import { ref } from 'vue';
 
 export default {
@@ -26,4 +26,20 @@ export default {
 
 <style scoped>
 
-</style>
+</style> -->
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// const msg = ref('Hello Vue3');
+const count = ref(0);
+function increment() {
+  console.log('increment', count.value);
+  count.value++;
+}
+</script>
+
+<template>
+  <h1 style="color: #333;">{{ count }}</h1>
+  <button @click="count++">{{ 'Add' }}</button>
+  <button @click="increment">increment</button>
+</template>
